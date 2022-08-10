7a6c54318658b5998993bf03fd66bbb19c2ba444
Duncan Brown <duncanjbrown@gmail.com>
2022-08-10 10:49:13 +0100

    Remove redundant controllers from dummy app

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def index
-    render plain: ''
-  end
-
-  private
-
-  def current_user
-    Struct.new(:id).new(1)
-  end
-
-  def current_namespace
-    'ddd'
-  end
 end
