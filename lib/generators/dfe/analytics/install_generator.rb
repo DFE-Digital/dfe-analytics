ba9085e49d1a2b7946fef8108f63fc5f5daa804d
Erica Porter <ericaporter@users.noreply.github.com>
2023-10-02 15:26:32 +0100

    testing installation

--- lib/generators/dfe/analytics/install_generator.rb
+++ lib/generators/dfe/analytics/install_generator.rb
@@ -1,9 +1,11 @@
 module DfE
   module Analytics
+    require 'pry'
     class InstallGenerator < ::Rails::Generators::Base
       namespace 'dfe:analytics:install'
 
       def install
+        binding.pry
         create_file 'config/initializers/dfe_analytics.rb', <<~FILE
           DfE::Analytics.configure do |config|
           #{indent(config_options.map(&:strip).join("\n\n").gsub(/# $/, '#').chomp.chomp, 2)}
