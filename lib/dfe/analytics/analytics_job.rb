b953e0cd20cec51366cf6df744d1efca6948ccd8
Peter Yates <peter.yates@graphia.co.uk>
2023-10-06 10:15:10 +0100

    Wait polynomially longer rather than exponentially
    `wait: :exponentially_longer` will actually wait polynomially longer and
    is therefore deprecated in Rails 7.1.0
    
    https://api.rubyonrails.org/classes/ActiveJob/Exceptions/ClassMethods.html#method-i-retry_on

--- lib/dfe/analytics/analytics_job.rb
+++ lib/dfe/analytics/analytics_job.rb
@@ -2,7 +2,7 @@ module DfE
   module Analytics
     class AnalyticsJob < ActiveJob::Base
       queue_as { DfE::Analytics.config.queue }
-      retry_on StandardError, wait: :exponentially_longer, attempts: 5
+      retry_on StandardError, wait: :polynomially_longer, attempts: 5
     end
   end
 end
