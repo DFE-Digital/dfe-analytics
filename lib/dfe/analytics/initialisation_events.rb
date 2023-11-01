c92f02b21ed0c1d74746834399e01dc239b00e69
Erica Porter <ericaporter@users.noreply.github.com>
2023-11-01 11:35:50 +0000

    No longer call job from initialisation.rb

--- lib/dfe/analytics/initialisation_events.rb
+++ lib/dfe/analytics/initialisation_events.rb
@@ -36,8 +36,6 @@ module DfE
           DfE::Analytics::SendEvents.perform_now([initialise_analytics_event])
         end
 
-        DfE::Analytics::EntityTableCheckJob.perform_async if defined?(Sidekiq) && DfE::Analytics.entity_table_checks_enabled?
-
         @@initialisation_events_sent = true # rubocop:disable Style:ClassVars
       end
 
