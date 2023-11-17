3ad193f11dd45e7451294d493c88e2114a68e109
Erica Porter <ericaporter@users.noreply.github.com>
2023-11-17 18:19:10 +0000

    Update lookup to SQL if postgres db (#96)
    * Update lookup to SQL if Postgres db
    * Update Postgres checksum timestamp, add conditional for Postgres db on prod
    * Update timestamp to pass into both Postgres and SQLite3/other database
    * Handle row_count equals zero

--- lib/dfe/analytics/entity_table_check_job.rb
+++ lib/dfe/analytics/entity_table_check_job.rb
@@ -13,29 +13,81 @@ module DfE
 
         DfE::Analytics.entities_for_analytics.each do |entity_name|
           DfE::Analytics.models_for_entity(entity_name).each do |model|
-            entity_table_check_event = DfE::Analytics::Event.new
-                                                            .with_type('entity_table_check')
-                                                            .with_entity_table_name(model.table_name)
-                                                            .with_data(entity_table_check_data(model))
-                                                            .as_json
+            entity_table_check_event = build_event_for(model)
             DfE::Analytics::SendEvents.perform_later([entity_table_check_event])
             Rails.logger.info("Processing data for #{model.table_name} with row count #{model.count}")
           end
         end
       end
 
+      def build_event_for(model)
+        DfE::Analytics::Event.new
+          .with_type('entity_table_check')
+          .with_entity_table_name(model.table_name)
+          .with_data(entity_table_check_data(model))
+          .as_json
+      end
+
       def entity_table_check_data(model)
-        checksum_calculated_at = Time.now.in_time_zone(TIME_ZONE).iso8601(6)
-        table_ids =
-          model
-          .where('updated_at < ?', Time.parse(checksum_calculated_at))
-          .order(updated_at: :asc)
-          .pluck(:id)
+        adapter_name = ActiveRecord::Base.connection.adapter_name.downcase
+        return unless supported_adapter_and_environment?(adapter_name)
+
+        checksum_calculated_at = fetch_current_timestamp_in_time_zone
+
+        row_count, checksum = fetch_checksum_data(model, adapter_name, checksum_calculated_at)
         {
-          row_count: table_ids.count,
-          checksum: Digest::SHA256.hexdigest(table_ids.join),
+          row_count: row_count,
+          checksum: checksum,
           checksum_calculated_at: checksum_calculated_at
         }
+      end
+
+      def supported_adapter_and_environment?(adapter_name)
+        if adapter_name != 'postgresql' && Rails.env.production?
+          Rails.logger.info('DfE::Analytics: Entity checksum: Only Postgres databases supported on PRODUCTION')
+          return false
+        end
+        true
+      end
+
+      def fetch_current_timestamp_in_time_zone
+        result = ActiveRecord::Base.connection.select_all('SELECT CURRENT_TIMESTAMP AS current_timestamp')
+        result.first['current_timestamp'].in_time_zone(TIME_ZONE).iso8601(6)
+      end
+
+      def fetch_checksum_data(model, adapter_name, checksum_calculated_at)
+        if adapter_name == 'postgresql'
+          fetch_postgresql_checksum_data(model, checksum_calculated_at)
+        else
+          fetch_generic_checksum_data(model, checksum_calculated_at)
+        end
+      end
+
+      def fetch_postgresql_checksum_data(model, checksum_calculated_at)
+        sanitized_table_name = ActiveRecord::Base.connection.quote_table_name(model.table_name)
+        checksum_calculated_at_sanitized = ActiveRecord::Base.connection.quote(checksum_calculated_at)
+        checksum_sql_query = <<-SQL
+          SELECT COUNT(*) as row_count,
+            MD5(COALESCE(STRING_AGG(CHECKSUM_TABLE.ID, '' ORDER BY CHECKSUM_TABLE.UPDATED_AT ASC), '')) as checksum
+          FROM (
+            SELECT #{sanitized_table_name}.id::TEXT as ID,
+                   #{sanitized_table_name}.updated_at as UPDATED_AT
+            FROM #{sanitized_table_name}
+            WHERE #{sanitized_table_name}.updated_at < #{checksum_calculated_at_sanitized}
+          ) CHECKSUM_TABLE
+        SQL
+
+        result = ActiveRecord::Base.connection.execute(checksum_sql_query).first
+        [result['row_count'].to_i, result['checksum']]
+      end
+
+      def fetch_generic_checksum_data(model, checksum_calculated_at)
+        table_ids =
+          model
+          .where('updated_at < ?', checksum_calculated_at)
+          .order(updated_at: :asc)
+          .pluck(:id)
+        [table_ids.count, Digest::MD5.hexdigest(table_ids.join)]
       end
     end
   end
--- spec/dfe/analytics/entity_table_check_job_spec.rb
+++ spec/dfe/analytics/entity_table_check_job_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe DfE::Analytics::EntityTableCheckJob do
     let(:wait_time) { Date.tomorrow.midnight }
     let(:time_now) { Time.new(2023, 9, 19, 12, 0, 0) }
     let(:time_zone) { 'London' }
-    let(:checksum_calculated_at) { time_now.in_time_zone(time_zone).iso8601(6) }
+    let(:checksum_calculated_at) { ActiveRecord::Base.connection.select_all('SELECT CURRENT_TIMESTAMP AS current_timestamp').first['current_timestamp'].in_time_zone('London').iso8601(6) }
 
     it 'does not run if entity table check is disabled' do
       DfE::Analytics.config.entity_table_checks_enabled = false
@@ -41,14 +41,33 @@ RSpec.describe DfE::Analytics::EntityTableCheckJob do
 
     it 'sends the entity_table_check event to BigQuery' do
       [123, 124, 125].map { |id| Candidate.create(id: id) }
-      table_ids = Candidate.where('updated_at < ?', Time.parse(checksum_calculated_at)).order(updated_at: :asc).pluck(:id)
-      checksum = Digest::SHA256.hexdigest(table_ids.join)
+      table_ids = Candidate.where('updated_at < ?', checksum_calculated_at).order(updated_at: :asc).pluck(:id)
+      checksum = Digest::MD5.hexdigest(table_ids.join)
       described_class.new.perform
 
       expect(DfE::Analytics::SendEvents).to have_received(:perform_later)
         .with([a_hash_including({
-          'entity_table_name' => Candidate.table_name,
-          'event_type' => 'entity_table_check',
+          'data' =>
+          [
+            { 'key' => 'row_count', 'value' => [table_ids.size] },
+            { 'key' => 'checksum', 'value' => [checksum] },
+            { 'key' => 'checksum_calculated_at', 'value' => [checksum_calculated_at] }
+          ]
+        })])
+    end
+
+    it 'does not send the event if updated_at is greater than checksum_calculated_at' do
+      parsed_time = DateTime.parse(checksum_calculated_at)
+      Candidate.create(id: '123', updated_at: parsed_time - 2.hours)
+      Candidate.create(id: '124', updated_at: parsed_time - 5.hours)
+      Candidate.create(id: '125', updated_at: parsed_time + 5.hours)
+
+      table_ids = Candidate.where('updated_at < ?', checksum_calculated_at).order(updated_at: :asc).pluck(:id)
+      checksum = Digest::MD5.hexdigest(table_ids.join)
+      described_class.new.perform
+
+      expect(DfE::Analytics::SendEvents).to have_received(:perform_later)
+        .with([a_hash_including({
           'data' => [
             { 'key' => 'row_count', 'value' => [table_ids.size] },
             { 'key' => 'checksum', 'value' => [checksum] },
@@ -57,22 +76,15 @@ RSpec.describe DfE::Analytics::EntityTableCheckJob do
       })])
     end
 
-    it 'does not send the event if updated_at is greater than checksum_calculated_at' do
-      checksum_calculated_at = Time.parse(time_now.in_time_zone(time_zone).iso8601(6))
-      Candidate.create(id: '123', updated_at: checksum_calculated_at - 2.hours)
-      Candidate.create(id: '124', updated_at: checksum_calculated_at - 5.hours)
-      Candidate.create(id: '125', updated_at: checksum_calculated_at + 5.hours)
-
+    it 'returns zero rows and checksum if table is empty' do
       table_ids = Candidate.where('updated_at < ?', checksum_calculated_at).order(updated_at: :asc).pluck(:id)
-      checksum = Digest::SHA256.hexdigest(table_ids.join)
+      checksum = Digest::MD5.hexdigest(table_ids.join)
       described_class.new.perform
 
       expect(DfE::Analytics::SendEvents).to have_received(:perform_later)
         .with([a_hash_including({
-          'entity_table_name' => Candidate.table_name,
-          'event_type' => 'entity_table_check',
           'data' => [
-            { 'key' => 'row_count', 'value' => [table_ids.size] },
+            { 'key' => 'row_count', 'value' => [0] },
             { 'key' => 'checksum', 'value' => [checksum] },
             { 'key' => 'checksum_calculated_at', 'value' => [checksum_calculated_at] }
           ]
