c5dd5495152a9024266f7ccd4eb8482e0452944c
Thomas Leese <thomas@leese.io>
2024-02-13 15:17:14 +0000

    Use original_fullpath for request path
    This changes the event to get the request path from the
    `original_fullpath` method rather than the `path` method. I'm making
    this change as the `original_fullpath` represents the path of the page
    as requested by the user, not necessarily the page being rendered.
    
    In most cases the original path and the path will be the same, but there
    are some scenarios where it is. Specifically, when rendering a custom
    error page in Rails which is mounted at `/404` the path ends up being
    the value `/404` rather than the path which triggered the 404 page.

--- lib/dfe/analytics/event.rb
+++ lib/dfe/analytics/event.rb
@@ -36,7 +36,7 @@ module DfE
           request_uuid: rack_request.uuid,
           request_user_agent: ensure_utf8(rack_request.user_agent),
           request_method: rack_request.method,
-          request_path: ensure_utf8(rack_request.path),
+          request_path: ensure_utf8(rack_request.original_fullpath.split('?').first),
           request_query: hash_to_kv_pairs(Rack::Utils.parse_query(rack_request.query_string)),
           request_referer: ensure_utf8(rack_request.referer),
           anonymised_user_agent_and_ip: anonymised_user_agent_and_ip(rack_request)
