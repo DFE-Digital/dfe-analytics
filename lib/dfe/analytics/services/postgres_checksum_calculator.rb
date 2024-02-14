b67d588719961ff6a8ce5a5b874448b9376be436
Erica Porter <ericaporter@users.noreply.github.com>
2024-02-14 22:15:07 +0000

    Further testing updates

--- lib/dfe/analytics/services/postgres_checksum_calculator.rb
+++ lib/dfe/analytics/services/postgres_checksum_calculator.rb
@@ -29,14 +29,14 @@ module DfE
           table_name_sanitized = connection.quote_table_name(entity)
           checksum_calculated_at_sanitized = connection.quote(checksum_calculated_at)
           where_clause = build_where_clause(order_column, table_name_sanitized, checksum_calculated_at_sanitized)
-          order_column_sanitized = connection.quote_column_name(order_column.downcase)
+          order_column_sanitized = order_column.downcase
 
           checksum_sql_query = <<-SQL
           SELECT COUNT(*) as row_count,
             MD5(COALESCE(STRING_AGG(CHECKSUM_TABLE.ID, '' ORDER BY CHECKSUM_TABLE.#{order_column_sanitized} ASC), '')) as checksum
           FROM (
             SELECT #{table_name_sanitized}.id::TEXT as ID,
-                  #{table_name_sanitized}.#{order_column_sanitized} as order_column_value
+                  #{table_name_sanitized}.#{order_column_sanitized} as order_column_alias
             FROM #{table_name_sanitized}
             #{where_clause}
           ) AS CHECKSUM_TABLE
