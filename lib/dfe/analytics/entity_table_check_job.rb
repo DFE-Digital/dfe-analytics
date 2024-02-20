a925362b48b9745d8704bf556e1399af2e70d74e
Erica Porter <ericaporter@users.noreply.github.com>
2024-02-20 15:30:51 +0000

    Add non null entity_tag value

--- lib/dfe/analytics/entity_table_check_job.rb
+++ lib/dfe/analytics/entity_table_check_job.rb
@@ -9,8 +9,9 @@ module DfE
       def perform
         return unless DfE::Analytics.entity_table_checks_enabled?
 
+        entity_tag = Time.now.strftime('%Y%m%d%H%M%S')
         DfE::Analytics.entities_for_analytics.each do |entity_name|
-          DfE::Analytics::Services::EntityTableChecks.call(entity_name: entity_name, entity_type: 'entity_table_check', entity_tag: [])
+          DfE::Analytics::Services::EntityTableChecks.call(entity_name: entity_name, entity_type: 'entity_table_check', entity_tag: entity_tag)
         end
       end
     end
