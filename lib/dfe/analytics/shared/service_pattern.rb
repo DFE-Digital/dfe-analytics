8278e9c3d7bf1e2394fc3b04dfe909f0c45cea2a
Erica Porter <ericaporter@users.noreply.github.com>
2024-06-24 15:28:39 +0100

    Fix for service pattern namespacing issue (#137)

--- lib/dfe/analytics/shared/service_pattern.rb
+++ lib/dfe/analytics/shared/service_pattern.rb
@@ -1,23 +1,27 @@
-# a template for creating service objects
-module ServicePattern
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.class_eval do
-      private_class_method(:new)
-    end
-  end
+module DfE
+  module Analytics
+    # a template for creating service objects
+    module ServicePattern
+      def self.included(base)
+        base.extend(ClassMethods)
+        base.class_eval do
+          private_class_method(:new)
+        end
+      end
 
-  def call
-    raise(NotImplementedError('#call must be implemented'))
-  end
+      def call
+        raise(NotImplementedError('#call must be implemented'))
+      end
 
-  # provides class-level methods to the classes that include ServicePattern
-  # defines a template for the `call` method; the expected entry point for service objects
-  module ClassMethods
-    def call(*args, **keyword_args, &block)
-      return new.call if args.empty? && keyword_args.empty? && block.nil?
+      # provides class-level methods to the classes that include ServicePattern
+      # defines a template for the `call` method; the expected entry point for service objects
+      module ClassMethods
+        def call(*args, **keyword_args, &block)
+          return new.call if args.empty? && keyword_args.empty? && block.nil?
 
-      new(*args, **keyword_args, &block).call
+          new(*args, **keyword_args, &block).call
+        end
+      end
     end
   end
 end
