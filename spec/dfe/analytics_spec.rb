c84b132c623c03fd65f9e82ac91b79afba5656bf
Marc Sardon <marc@bichito.software>
2024-12-04 01:44:42 +0000

    Fix "async" config setup
    DfE::Analytics initialisation code was ignoring the
    "config.async = false" option when provided, and setting it to
    "config.async = true" anyways.
    
    This was caused by the conditional assignment operator "||=".
    
    "foo ||= true" does set 'foo' as 'true' if it was either 'nil' or
    'false'.

--- spec/dfe/analytics_spec.rb
+++ spec/dfe/analytics_spec.rb
@@ -189,6 +189,28 @@ RSpec.describe DfE::Analytics do
     end
   end
 
+  describe '.async?' do
+    context 'when the async config has been set to true' do
+      before do
+        described_class.configure { |config| config.async = true }
+      end
+
+      it 'returns true' do
+        expect(described_class.async?).to be true
+      end
+    end
+
+    context 'when the async config has been set to false' do
+      before do
+        described_class.configure { |config| config.async = false }
+      end
+
+      it 'returns false' do
+        expect(described_class.async?).to be false
+      end
+    end
+  end
+
   describe '.extract_model_attributes' do
     with_model :Candidate do
       table do |t|
