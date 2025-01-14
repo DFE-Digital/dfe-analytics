1b1c74e821768db0a47968ce12232bc1d67dc331
Max Mills <8balldigitalsolutions@gmail.com>
2025-01-14 17:48:20 +0000

    initialize transaction changes

--- spec/dfe/analytics/transaction_changes_spec.rb
+++ spec/dfe/analytics/transaction_changes_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe DfE::Analytics::TransactionChanges do
+  let(:allowlist_fields) { [] }
+  let(:hidden_pii_fields) { [] }
+
   with_model :Candidate do
     table do |t|
       t.string :email_address
@@ -10,7 +13,6 @@ RSpec.describe DfE::Analytics::TransactionChanges do
     end
 
     model do
-      include DfE::Analytics::TransactionChanges
       attr_accessor :stored_transaction_changes
 
       after_commit :store_transaction_changes_for_tests
@@ -24,7 +26,23 @@ RSpec.describe DfE::Analytics::TransactionChanges do
     end
   end
 
+  before do
+    allow(DfE::Analytics).to receive(:allowlist).and_return({
+      Candidate.table_name.to_sym => allowlist_fields
+    })
+
+    allow(DfE::Analytics).to receive(:hidden_pii).and_return({
+      Candidate.table_name.to_sym => hidden_pii_fields
+    })
+
+    # autogenerate a compliant blocklist
+    allow(DfE::Analytics).to receive(:blocklist).and_return(DfE::Analytics::Fields.generate_blocklist)
+
+    DfE::Analytics.initialize!
+  end
+
   describe 'after commit' do
+    let(:allowlist_fields) { %w[id email_address] }
     context 'without transaction' do
       it 'create tracks changes to attributes' do
         entity = Candidate.create(email_address: 'foo@bar')
@@ -51,11 +69,15 @@ RSpec.describe DfE::Analytics::TransactionChanges do
 
       it 'update tracks changes to attributes' do
         entity = Candidate.create(email_address: 'foo@bar')
+        entity1 = Candidate.create(email_address: 'doo@bar')
+
         ActiveRecord::Base.transaction do
           entity.update(email_address: 'bar@baz')
+          entity1.update(email_address: 'jar@baz')
         end
 
         expect(entity.stored_transaction_changes).to eq({ 'email_address' => ['foo@bar', 'bar@baz'] })
+        expect(entity1.stored_transaction_changes).to eq({ 'email_address' => ['doo@bar', 'jar@baz'] })
       end
     end
 
