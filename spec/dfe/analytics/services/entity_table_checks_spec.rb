e6908e2fa43dbc4ddc9913e85f3e8dba38b50315
Max Mills <8balldigitalsolutions@gmail.com>
2025-06-09 11:05:18 +0100

    update entity table checks

--- spec/dfe/analytics/services/entity_table_checks_spec.rb
+++ spec/dfe/analytics/services/entity_table_checks_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
 
   describe '#call' do
     let(:time_zone) { 'London' }
-    let(:order_column) { 'UPDATED_AT' }
+    let(:order_column) { 'CREATED_AT' }
     let(:course_entity) { DfE::Analytics.entities_for_analytics.find { |entity| entity.to_s.include?('course') } }
     let(:institution_entity) { DfE::Analytics.entities_for_analytics.find { |entity| entity.to_s.include?('institution') } }
     let(:application_entity) { DfE::Analytics.entities_for_analytics.find { |entity| entity.to_s.include?('application') } }
@@ -97,11 +97,11 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
       expect(Rails.logger).to have_received(:info).with(expected_message)
     end
 
-    it 'uses updated_at if it exists and has no null values' do
-      Candidate.create!(id: 1, email_address: 'first@example.com', updated_at: Time.current)
-      Candidate.create!(id: 2, email_address: 'second@example.com', updated_at: Time.current + 1.minute)
+    it 'uses created_at if it exists and has no null values' do
+      Candidate.create!(id: 1, email_address: 'first@example.com')
+      Candidate.create!(id: 2, email_address: 'second@example.com')
 
-      table_ids = Candidate.order(:updated_at).pluck(:id)
+      table_ids = Candidate.order(:created_at).pluck(:id)
       checksum = Digest::MD5.hexdigest(table_ids.join)
 
       described_class.call(entity_name: candidate_entity, entity_type: entity_type, entity_tag: nil)
@@ -112,12 +112,12 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
             { 'key' => 'row_count', 'value' => [table_ids.size] },
             { 'key' => 'checksum', 'value' => [checksum] },
             { 'key' => 'checksum_calculated_at', 'value' => [checksum_calculated_at] },
-            { 'key' => 'order_column', 'value' => ['UPDATED_AT'] }
+            { 'key' => 'order_column', 'value' => [order_column] }
           ]
       })])
     end
 
-    it 'falls back to id when both updated_at and created_at are null' do
+    it 'falls back to id when created_at is null' do
       frozen_time = Time.zone.now.in_time_zone('London')
       Timecop.freeze(frozen_time)
 
@@ -125,9 +125,9 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
       candidate2 = Candidate.create!(id: 2, email_address: 'second@example.com')
       candidate3 = Candidate.create!(id: 3, email_address: 'third@example.com')
 
-      candidate1.update_columns(created_at: nil, updated_at: nil)
-      candidate2.update_columns(created_at: nil, updated_at: nil)
-      candidate3.update_columns(created_at: nil, updated_at: nil)
+      candidate1.update_columns(created_at: nil)
+      candidate2.update_columns(created_at: nil)
+      candidate3.update_columns(created_at: nil)
 
       table_ids = Candidate.order(:id).pluck(:id)
       checksum = Digest::MD5.hexdigest(table_ids.join)
@@ -147,13 +147,13 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
       Timecop.return
     end
 
-    it 'falls back to created_at when updated_at is null but created_at exists' do
+    it 'defaults to created_at when updated_at is null but created_at exists' do
       Candidate.create!(id: 1, email_address: 'first@example.com', updated_at: nil, created_at: 2.hours.ago)
       Candidate.create!(id: 2, email_address: 'second@example.com', updated_at: nil, created_at: 1.hour.ago)
       Candidate.create!(id: 3, email_address: 'third@example.com', updated_at: nil, created_at: 3.hours.ago)
 
-      # Ensure updated_at is nil
-      Candidate.update_all(updated_at: nil)
+      # Ensure updated_at is not nil
+      Candidate.update_all(updated_at: Time.now)
 
       table_ids = Candidate.order(:created_at).pluck(:id)
       checksum = Digest::MD5.hexdigest(table_ids.join)
@@ -195,7 +195,7 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
     it 'sends an entity table check event' do
       [130, 131, 132].map { |id| Candidate.create(id: id) }
       candidate_entities = DfE::Analytics.entities_for_analytics.select { |entity| entity.to_s.include?('candidate') }
-      table_ids = Candidate.where('updated_at < ?', checksum_calculated_at).order(updated_at: :asc).pluck(:id)
+      table_ids = Candidate.where('created_at < ?', checksum_calculated_at).order(created_at: :asc).pluck(:id)
       checksum = Digest::MD5.hexdigest(table_ids.join)
 
       candidate_entities.each do |candidate|
@@ -214,7 +214,7 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
     end
 
     it 'returns zero rows and checksum if table is empty' do
-      table_ids = Candidate.where('updated_at < ?', checksum_calculated_at).order(updated_at: :asc).pluck(:id)
+      table_ids = Candidate.where('created_at < ?', checksum_calculated_at).order(created_at: :asc).pluck(:id)
       checksum = Digest::MD5.hexdigest(table_ids.join)
       described_class.call(entity_name: candidate_entity, entity_type: entity_type, entity_tag: nil)
 
@@ -249,7 +249,7 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
       end
     end
 
-    it 'orders by id if created_at and updated_at are missing for Institution' do
+    it 'orders by id if created_at is missing for Institution' do
       order_column = 'ID'
 
       ['Institute A', 'Institute B', 'Institute C'].each { |name| Institution.create(name: name, address: 'Some address') }
@@ -273,14 +273,14 @@ RSpec.describe DfE::Analytics::Services::EntityTableChecks do
       end
     end
 
-    it 'orders records by updated_at truncated to milliseconds' do
+    it 'orders records by created_at truncated to milliseconds' do
       time_base = Time.zone.now.beginning_of_minute
-      Candidate.create(email_address: 'first@example.com', updated_at: time_base + 0.001.seconds)
-      Candidate.create(email_address: 'second@example.com', updated_at: time_base + 0.005.seconds)
+      Candidate.create(email_address: 'first@example.com')
+      Candidate.create(email_address: 'second@example.com')
 
       described_class.call(entity_name: candidate_entity, entity_type: entity_type, entity_tag: nil)
 
-      ordered_candidates = Candidate.order(:updated_at).pluck(:email_address)
+      ordered_candidates = Candidate.order(:created_at).pluck(:email_address)
 
       expect(ordered_candidates).to eq(['first@example.com', 'second@example.com'])
     end
