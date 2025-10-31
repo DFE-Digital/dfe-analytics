ac818a173129b5b9702f7f38a6ab4be4f0e60417
Max Mills <8balldigitalsolutions@gmail.com>
2025-10-31 09:32:46 +0000

    update tests

--- spec/dfe/analytics/send_events_spec.rb
+++ spec/dfe/analytics/send_events_spec.rb
@@ -46,10 +46,10 @@ RSpec.describe DfE::Analytics::SendEvents do
 
       it 'logs events with all sensitive data masked' do
         expect(Rails.logger).to receive(:info) do |log_message|
-          expect(log_message).to include('"key"=>"dob", "value"=>["HIDDEN"]')
-          expect(log_message).to include('"key"=>"first_name", "value"=>["HIDDEN"]')
-          expect(log_message).to include('"key"=>"email", "value"=>"user@example.com"')
-          expect(log_message).to include('"key"=>"phone_number", "value"=>"1234567890"')
+          expect(log_message).to include('{"key" => "dob", "value" => ["HIDDEN"]}')
+          expect(log_message).to include('{"key" => "first_name", "value" => ["HIDDEN"]}')
+          expect(log_message).to include('{"key" => "email", "value" => "user@example.com"}')
+          expect(log_message).to include('{"key" => "phone_number", "value" => "1234567890"}')
         end
 
         described_class.new.perform([hidden_pii_event])
@@ -121,10 +121,10 @@ RSpec.describe DfE::Analytics::SendEvents do
 
       it 'masks sensitive data in the log output' do
         expect(Rails.logger).to receive(:info) do |log_message|
-          expect(log_message).to include('"key"=>"dob", "value"=>["HIDDEN"]')
-          expect(log_message).to include('"key"=>"first_name", "value"=>["HIDDEN"]')
-          expect(log_message).to include('"key"=>"email", "value"=>"user@example.com"')
-          expect(log_message).to include('"key"=>"phone_number", "value"=>"1234567890"')
+          expect(log_message).to include('{"key" => "dob", "value" => ["HIDDEN"]}')
+          expect(log_message).to include('{"key" => "first_name", "value" => ["HIDDEN"]}')
+          expect(log_message).to include('{"key" => "email", "value" => "user@example.com"}')
+          expect(log_message).to include('{"key" => "phone_number", "value" => "1234567890"}')
         end
 
         described_class.new.perform([hidden_pii_event])
