91080a41b5b90bf8c250fbc82e1592d446664099
Max Mills <8balldigitalsolutions@gmail.com>
2025-11-03 09:39:38 +0000

    rubocop

--- spec/dfe/analytics/send_events_spec.rb
+++ spec/dfe/analytics/send_events_spec.rb
@@ -46,10 +46,10 @@ RSpec.describe DfE::Analytics::SendEvents do
 
       it 'logs events with all sensitive data masked' do
         expect(Rails.logger).to receive(:info) do |log_message|
-          #expect(log_message).to include('{"key" => "dob", "value" => ["HIDDEN"]}')
-          #expect(log_message).to include('{"key" => "first_name", "value" => ["HIDDEN"]}')
-          #expect(log_message).to include('{"key" => "email", "value" => "user@example.com"}')
-          #expect(log_message).to include('{"key" => "phone_number", "value" => "1234567890"}')
+          # expect(log_message).to include('{"key" => "dob", "value" => ["HIDDEN"]}')
+          # expect(log_message).to include('{"key" => "first_name", "value" => ["HIDDEN"]}')
+          # expect(log_message).to include('{"key" => "email", "value" => "user@example.com"}')
+          # expect(log_message).to include('{"key" => "phone_number", "value" => "1234567890"}')
           expect(log_message).to include('["HIDDEN"]')
           expect(log_message).to include('"key" => "dob"')
           expect(log_message).to include('"key" => "first_name"')
